720ef9cd2a4781b9114b2c41b6a33376e6541179
Philip Andersson <philip.andersson.1991@hotmail.com>
2025-09-05 14:57:22 +0200

    fixade tooltip variabler

--- src/components/CustomToolTip.jsx
+++ src/components/CustomToolTip.jsx
@@ -1,40 +1,41 @@
 import React from "react";
-import { Tooltip, tooltipClasses} from "@mui/material";
-import { styled } from '@mui/material/styles';
-import InfoIcon from '@mui/icons-material/Info';
+import { IconButton, Tooltip, tooltipClasses } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import InfoIcon from "@mui/icons-material/Info";
 
 const CustomToolTip = ({ toolTipText = "" }) => {
-  const LightTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} classes={{ popper: className }} />
-  ))(({ theme }) => ({
-    [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: theme.palette.common.white,
-      color: "rgba(0, 0, 0, 0.87)",
-      boxShadow: theme.shadows[1],
-      fontSize: 14,
-      minWidth: {
-        xs: 300,
-        sm: 400,
-        md: 500,
-        lg: 600,
-      },
-      whiteSpace: "pre-line !important",
-    },
-  }));
+    const LightTooltip = styled(({ className, ...props }) => (
+        <Tooltip {...props} classes={{ popper: className }} />
+    ))(({ theme }) => ({
+        [`& .${tooltipClasses.tooltip}`]: {
+            backgroundColor: theme.palette.common.white,
+            color: "rgba(0, 0, 0, 0.87)",
+            boxShadow: theme.shadows[1],
+            fontSize: 14,
+            minWidth: {
+                xs: 300,
+                sm: 400,
+                md: 500,
+                lg: 600,
+            },
+            whiteSpace: "pre-line !important",
+        },
+    }));
 
-  return (
-    <LightTooltip 
-      title={toolTipText} 
-      placement="top-end"
-      arrow
-      enterTouchDealay={0}
-      leaveTouchdeDelay={4000}
-      disableInteractive={false}
-    >
-      <InfoIcon sx={{fontSize: 18, marginLeft: 1}}/>
-    </LightTooltip>
-  );
+    return (
+        <LightTooltip
+            title={toolTipText}
+            placement="top-end"
+            arrow
+            enterTouchDealay={0}
+            leaveTouchdeDelay={4000}
+            disableInteractive={false}
+        >
+            <IconButton size="small" edge="start" aria-label="Info">
+                <InfoIcon sx={{ fontSize: 18, marginLeft: 1 }} />
+            </IconButton>
+        </LightTooltip>
+    );
 };
 
 export default CustomToolTip;
-
